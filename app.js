8b4029117bf8aed4be87ee3db0921ebe2983b2f8
ghaidaabenrekia-dot <ghaidaa.benrekia@codedbykids.org>
2026-04-22 23:00:24 +0000

    finished resturant making

--- app.js
+++ app.js
@@ -1,2 +1,34 @@
-let name= "ghaidaa";
-console.log(name);
+let fooditem1= "fries";
+console.log(fooditem1);
+
+let fooditem2="burger";
+console.log(fooditem2);
+
+let fooditem3="pizza";
+console.log(fooditem3)
+
+let fooditem4="fish"
+console.log(fooditem4)
+
+let price1=2
+console.log(price1)
+
+let price2=10
+console.log(price2)
+
+let price3=25
+console.log(price3)
+
+let price4=30
+console.log(price4)
+let cost=0;
+console.log(cost)
+let funds=15;
+console.log(funds)
+
+// order for pizza and fries; update cost variable
+
+let price= price3 + price1
+console.log("price: " + price)
+
+console.log(price>funds)
